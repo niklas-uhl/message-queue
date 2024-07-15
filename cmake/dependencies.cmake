661762dbe0ed95cfda06ba7d1bb7059d4b3a9705
Tim Niklas Uhl <uhl@kit.edu>
2024-07-15 14:20:21 +0200

    Update KASSERT.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -11,7 +11,7 @@ FetchContent_Declare(
 FetchContent_Declare(
   kassert
   GIT_REPOSITORY https://github.com/kamping-site/kassert.git
-  GIT_TAG e683aef
+  GIT_TAG f0873f8
   SYSTEM
   OVERRIDE_FIND_PACKAGE
 )
