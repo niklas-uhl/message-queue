c27c06bb3c40c2b12ae67b77261dd23403b23432
Matthias Schimek <matthias.schimek@kit.edu>
2024-07-24 09:38:32 +0200

    add progress hook to indirect message queue

--- include/message-queue/indirection.hpp
+++ include/message-queue/indirection.hpp
@@ -138,7 +138,15 @@ public:
     /// (not necessarily the underlying data) is moved to the handler when
     /// called.
     bool terminate(MessageHandler<typename queue_type::message_type> auto&& on_message) {
-        return queue_type::terminate(redirection_handler(on_message));
+        return terminate(std::forward<decltype(on_message)>(on_message), []() {});
+    }
+
+    /// Note: Message handlers take a MessageEnvelope as single argument. The Envelope
+    /// (not necessarily the underlying data) is moved to the handler when
+    /// called.
+    bool terminate(MessageHandler<typename queue_type::message_type> auto&& on_message,
+                   std::invocable<> auto&& progress_hook) {
+        return queue_type::terminate(redirection_handler(on_message), progress_hook);
     }
 
 private:
