06ac2423e3e41a5a0fc2989baa24a563ef3e22cb
Tim Niklas Uhl <uhl@kit.edu>
2024-09-30 14:08:46 +0200

    Use FetchContent correctly.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -5,7 +5,6 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/kamping-site/kamping.git
   GIT_TAG v0.1.1
   SYSTEM
-  OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_Declare(
@@ -13,34 +12,31 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/kamping-site/kassert.git
   GIT_TAG f0873f8
   SYSTEM
-  OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_Declare(
   range-v3
   URL https://github.com/ericniebler/range-v3/archive/0.12.0.zip
-  OVERRIDE_FIND_PACKAGE
   SYSTEM
   SOURCE_SUBDIR NON_EXISTANT
 )
 
-if(NOT EXISTS ${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/range-v3-extra.cmake AND
-    NOT EXISTS ${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/range-v3Extra.cmake)
-  file(WRITE ${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/range-v3-extra.cmake
-[=[
-    add_library(range-v3 INTERFACE IMPORTED)
-    target_include_directories(range-v3
-                               INTERFACE ${range-v3_SOURCE_DIR}/include)
-    add_library(range-v3::range-v3 ALIAS range-v3)
-]=])
-endif()
+# if(NOT EXISTS ${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/range-v3-extra.cmake AND
+#     NOT EXISTS ${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/range-v3Extra.cmake)
+#   file(WRITE ${CMAKE_FIND_PACKAGE_REDIRECTS_DIR}/range-v3-extra.cmake
+# [=[
+#     add_library(range-v3 INTERFACE IMPORTED)
+#     target_include_directories(range-v3
+#                                INTERFACE ${range-v3_SOURCE_DIR}/include)
+#     add_library(range-v3::range-v3 ALIAS range-v3)
+# ]=])
+# endif()
 
 FetchContent_Declare(
   CLI11
   GIT_REPOSITORY https://github.com/CLIUtils/CLI11.git
   GIT_TAG v2.3.2
   SYSTEM
-  OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_Declare(
@@ -48,7 +44,6 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
   GIT_TAG 10.1.1
   SYSTEM
-  OVERRIDE_FIND_PACKAGE
 )
 
 FetchContent_Declare(
@@ -56,5 +51,4 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/kamping-site/bakward-mpi.git
   GIT_TAG 89de113
   SYSTEM
-  OVERRIDE_FIND_PACKAGE
 )
